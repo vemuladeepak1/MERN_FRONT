cc06ba1d16740d6e65d142724d0afba46c69eef9
unknown <deepakvemula123@gmail.com>
2022-01-11 12:44:00 +0530

    first commit

---
+++ client/src/component/Profile.js
@@ -0,0 +1,363 @@
+import { useContext, useEffect, useState } from "react";
+import {
+  Button,
+  Grid,
+  Typography,
+  Modal,
+  Paper,
+  makeStyles,
+  TextField,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from "@material-ui/core";
+import axios from "axios";
+import ChipInput from "material-ui-chip-input";
+import FileUploadInput from "../lib/FileUploadInput";
+import DescriptionIcon from "@material-ui/icons/Description";
+import FaceIcon from "@material-ui/icons/Face";
+
+import { SetPopupContext } from "../App";
+
+import apiList from "../lib/apiList";
+
+const useStyles = makeStyles((theme) => ({
+  body: {
+    height: "inherit",
+  },
+  popupDialog: {
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    // padding: "30px",
+  },
+}));
+
+const MultifieldInput = (props) => {
+  const classes = useStyles();
+  const { education, setEducation } = props;
+
+  return (
+    <>
+      {education.map((obj, key) => (
+        <Grid item container className={classes.inputBox} key={key}>
+          <Grid item xs={6}>
+            <TextField
+              label={`Institution Name #${key + 1}`}
+              value={education[key].institutionName}
+              onChange={(event) => {
+                const newEdu = [...education];
+                newEdu[key].institutionName = event.target.value;
+                setEducation(newEdu);
+              }}
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <TextField
+              label="Start Year"
+              value={obj.startYear}
+              variant="outlined"
+              type="number"
+              onChange={(event) => {
+                const newEdu = [...education];
+                newEdu[key].startYear = event.target.value;
+                setEducation(newEdu);
+              }}
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <TextField
+              label="End Year"
+              value={obj.endYear}
+              variant="outlined"
+              type="number"
+              onChange={(event) => {
+                const newEdu = [...education];
+                newEdu[key].endYear = event.target.value;
+                setEducation(newEdu);
+              }}
+            />
+          </Grid>
+        </Grid>
+      ))}
+      <Grid item style={{ alignSelf: "center" }}>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() =>
+            setEducation([
+              ...education,
+              {
+                institutionName: "",
+                startYear: "",
+                endYear: "",
+              },
+            ])
+          }
+          className={classes.inputBox}
+        >
+          Add another institution details
+        </Button>
+      </Grid>
+    </>
+  );
+};
+
+const Profile = (props) => {
+  const classes = useStyles();
+  const setPopup = useContext(SetPopupContext);
+  const [userData, setUserData] = useState();
+  const [open, setOpen] = useState(false);
+
+  const [profileDetails, setProfileDetails] = useState({
+    name: "",
+    education: [],
+    skills: [],
+    resume: [],
+    profile: "",
+    email:"",
+    Filename:"",
+    employment: "Fresher"
+  });
+
+  const [education, setEducation] = useState([
+    {
+      institutionName: "",
+      startYear: "",
+      endYear: "",
+    },
+  ]);
+  const [Expemployment, setexpEmployment] = useState([
+    {
+      companyName:"perfex",
+      Designation:"developer",
+      yearsofExp:"3 Years",
+      currentSalary:"3.5 LPA",
+      expectedSalary:"5LPA",
+      noticePeriod:"30 Days"
+    }
+  ])
+
+  const handleInput = (key, value) => {
+    console.log(value)
+    setProfileDetails({
+      ...profileDetails,
+      [key]: value
+    });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    axios
+      .get(apiList.user, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setProfileDetails(response.data);
+        if (response.data.education.length > 0) {
+          setEducation(
+            response.data.education.map((edu) => ({
+              institutionName: edu.institutionName ? edu.institutionName : "",
+              startYear: edu.startYear ? edu.startYear : "",
+              endYear: edu.endYear ? edu.endYear : "",
+            }))
+          );
+        }
+      })
+      .catch((err) => {
+        // console.log(err.response.data);
+        setPopup({
+          open: true,
+          severity: "error",
+          message: "Error",
+        });
+      });
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const editDetails = () => {
+    setOpen(true);
+  };
+  const handleEmp = ()=>{
+      
+  }
+  const handleUpdate = () => {
+    console.log(education);
+    let updatedDetails = {
+      ...profileDetails,
+      education: education
+        .filter((obj) => obj.institutionName.trim() !== "")
+        .map((obj) => {
+          if (obj["endYear"] === "") {
+            delete obj["endYear"];
+          }
+          return obj;
+        }),
+        
+    };
+
+    axios
+      .put(apiList.user, updatedDetails, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((response) => {
+        setPopup({
+          open: true,
+          severity: "success",
+          message: response.data.message,
+        });
+        getData();
+      })
+      .catch((err) => {
+        setPopup({
+          open: true,
+          severity: "error",
+          message: err.response.data.message,
+        });
+        console.log(err.response);
+      });
+    setOpen(false);
+  };
+console.log(profileDetails.employment)
+  return (
+    <>
+      <Grid
+        container
+        item
+        direction="column"
+        alignItems="center"
+        style={{ padding: "30px", minHeight: "93vh" }}
+      >
+        <Grid item>
+          <Typography variant="h2">Profile</Typography>
+        </Grid>
+        <Grid item xs>
+          <Paper
+            style={{
+              padding: "20px",
+              outline: "none",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Grid container direction="column" alignItems="stretch" spacing={3}>
+              <Grid item>
+                <TextField
+                  label="Name"
+                  value={profileDetails.name}
+                  onChange={(event) => handleInput("name", event.target.value)}
+                  className={classes.inputBox}
+                  variant="outlined"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item>
+                <TextField
+                  label="Email"
+                  value={profileDetails.email}
+                  onChange={(event) => handleInput("Email", event.target.value)}
+                  className={classes.inputBox}
+                  variant="outlined"
+                  fullWidth
+                />
+              </Grid>
+              <MultifieldInput
+                education={education}
+                setEducation={setEducation}
+              />
+              <Grid item>
+                <ChipInput
+                  className={classes.inputBox}
+                  label="Skills"
+                  variant="outlined"
+                  helperText="Press enter to add skills"
+                  value={profileDetails.skills}
+                  onAdd={(chip) =>
+                    setProfileDetails({
+                      ...profileDetails,
+                      skills: [...profileDetails.skills, chip],
+                    })
+                  }
+                  onDelete={(chip, index) => {
+                    let skills = profileDetails.skills;
+                    skills.splice(index, 1);
+                    setProfileDetails({
+                      ...profileDetails,
+                      skills: skills,
+                    });
+                  }}
+                  fullWidth
+                />
+              </Grid>
+               <Grid item>
+               <FormControl component="fieldset" 
+               onChange={(event) => handleInput("employment", event.target.value)} >
+                  <FormLabel component="legend">Employment</FormLabel>
+                  <RadioGroup row aria-label="gender" name="row-radio-buttons-group"  value={profileDetails?.employment}>
+                    <FormControlLabel value="Fresher" control={<Radio />} label="Fresher" />
+                    <FormControlLabel value="Experienced" control={<Radio />} label="Experienced" />
+                  </RadioGroup>
+                </FormControl>
+               </Grid>
+              <Grid item>
+                {
+                  profileDetails.employment=="Experienced"? <div>Hello world</div> : null
+                }
+                <FileUploadInput
+                  className={classes.inputBox}
+                  label="Resume (.pdf)"
+                  icon={<DescriptionIcon />}
+                  uploadTo={apiList.uploadResume}
+                  handleInput={handleInput}
+                  identifier={"resume"}
+                 
+                />
+              </Grid>
+              <Grid item>
+                <FileUploadInput
+                  className={classes.inputBox}
+                  label="Profile Photo (.jpg/.png)"
+                  icon={<FaceIcon />}
+                  uploadTo={apiList.uploadProfileImage}
+                  handleInput={handleInput}
+                  identifier={"profile"}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ padding: "10px 50px", marginTop: "30px" }}
+              onClick={() => handleUpdate()}
+            >
+              Update Details
+            </Button>
+          </Paper>
+        </Grid>
+      </Grid>
+      {/* <Modal open={open} onClose={handleClose} className={classes.popupDialog}> */}
+
+      {/* </Modal> */}
+    </>
+  );
+};
+
+export default Profile;
---
+++ client/src/lib/apiList.js
@@ -0,0 +1,16 @@
+export const server = "http://31.220.48.248";
+
+const apiList = {
+  login: `${server}/auth/login`,
+  signup: `${server}/auth/signup`,
+  uploadResume: `${server}/upload/resume`,
+  uploadProfileImage: `${server}/upload/profile`,
+  jobs: `${server}/api/jobs`,
+  applications: `${server}/api/applications`,
+  rating: `${server}/api/rating`,
+  user: `${server}/api/user`,
+  applicants: `${server}/api/applicants`,
+  sendotp:`${server}/api/sendotp`
+};
+
+export default apiList;
